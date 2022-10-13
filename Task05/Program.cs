175a334199928d4e278d74ea6b1638db879e5287
denis <den_vbg@mail.ru>
2022-10-13 12:30:11 +0300

    task05

---
+++ Task05/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 5
+Ввод числа
+показываем числа в промежутке от -него до него
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>0) {
+    int count = -number;
+
+    while (count<=number)
+    {
+        
+        Console.Write($"{count}, ");
+        count ++;
+    }
+}
+else {
+
+ Console.WriteLine("Введите число больше 0");   
+}

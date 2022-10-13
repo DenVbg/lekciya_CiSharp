b4bd59167b0d361803413a6f1a1874bc3572cbb3
denis <den_vbg@mail.ru>
2022-10-13 15:37:35 +0300

    добавил решение Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,28 @@
+﻿//4. Напишите программу, которая на вход принимает три
+//числа и выдает, какое число большее
+//2,3,7 -> max=7
+//44,5,78 -> max=78
+//22,3,9 -> max=22
+
+
+Console.WriteLine("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;  
+}
+if (numberC > max)
+{
+    max = numberC;  
+}
+
+Console.WriteLine($"Максимальным из введенных числел {numberA}, {numberB}, {numberC}. является число - {max}");

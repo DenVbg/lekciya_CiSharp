a8680190b4bd4db49c669aaab6cc714d569a92d4
denis <den_vbg@mail.ru>
2022-10-13 16:23:49 +0300

    проверка, навел красоту

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -6,7 +6,7 @@ int N = Convert.ToInt32(Console.ReadLine());
 int count = 1;
 
 if (N<=0) {
-    Console.Write("Вы ввели число меньше либо равное 0.");
+    Console.Write("Вы ввели число меньше либо равное \"0\".");
 }
 else {
     Console.Write("Четные числа от 1 до N: ");

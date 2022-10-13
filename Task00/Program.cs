908e4d666fe1be6e0137d63a0579a8e7ca225d7b
denis <den_vbg@mail.ru>
2022-10-13 12:07:48 +0300

    добвалены task01 task03

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square  = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+bool result = =number == square; // true - false
+if (number > 5 || square && )
+{
+
+}
+else
+{
+    
+}

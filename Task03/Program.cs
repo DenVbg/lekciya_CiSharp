2233a20a017bd61154ba59138634783a94d41053
denis <den_vbg@mail.ru>
2022-10-14 18:21:25 +0300

    clear obj

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -10,26 +10,26 @@ if ( day > 0 && day < 8 ) {
     if (day==1) {
         Console.WriteLine("Понедельник");
     }
-    if (day==2) {
+    else if (day==2) {
         Console.WriteLine("Вторник");
     }
-    if (day==3) {
+    else if (day==3) {
         Console.WriteLine("Среда");
     }
-    if (day==4) {
+    else if (day==4) {
         Console.WriteLine("Четверг");
     }
-    if (day==5) {
+    else if (day==5) {
         Console.WriteLine("Пятница");
     }
-    if (day==6) {
+    else if (day==6) {
         Console.WriteLine("Суббота");
     }
-    if (day==7) {
+    else if (day==7) {
         Console.WriteLine("Воскресенье");
     }
 }
 else 
 {
-    Console.WriteLine("Заданное число не соотвествует дням недели");
+    Console.WriteLine("Заданное число не соотвествует дню недели");
 }

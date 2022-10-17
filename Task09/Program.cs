6202f4cb9d8bf62286ab6c1d3758507b0eed75c1
denis <den_vbg@mail.ru>
2022-10-17 10:33:52 +0300

    edit Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,5 +9,5 @@ int number = new Random().Next(10,100);
 Console.WriteLine($"Случайное число от 10 до 99: {number}");
 int num1 = number / 10;
 int num2 = number % 10;
-if (num1>num2) Console.WriteLine($"max = {num1}, min = {num2} "); 
-else Console.WriteLine($"max = {num2}, min = {num1} ");  
+if (num1>num2) Console.WriteLine($"наибольшая цифра числа {num1}, min = {num2} "); 
+else Console.WriteLine($"наибольшая цифра числа {num2}, min = {num1} ");  

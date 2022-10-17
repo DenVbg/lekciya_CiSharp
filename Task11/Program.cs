4a5ea1125045dadff78980a838b21fb46733070c
denis <den_vbg@mail.ru>
2022-10-17 12:46:40 +0300

    add Task11 Task12 Task14 Task16

---
+++ Task11/Program.cs
@@ -0,0 +1,35 @@
+﻿// 1. вход случайное трехзначное число
+// 2. удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// 1.Вариант
+//int number = new Random().Next(100,1000);
+//Console.WriteLine($"Случайное число от 100 до 999: {number}");
+
+// int firstDigit = number / 100;
+// // Console.WriteLine($"1 цифра: {firstDigit}");
+// int thirdDigit = number % 10;
+// // Console.WriteLine($"3 цифра: {thirdDigit}");
+
+// int result = firstDigit*10 + thirdDigit;
+// Console.WriteLine($"вывод: {result}");
+
+// 2.Вариант
+int DelSecondDigit(int num) //num=number
+{
+    int firstDigit = num / 100;
+    //Console.WriteLine($"1 цифра: {firstDigit}");
+    int thirdDigit = num % 10;
+    //Console.WriteLine($"3 цифра: {thirdDigit}");
+    int result = firstDigit*10 + thirdDigit;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число от 100 до 999: {number}");
+int result = DelSecondDigit(number);
+
+Console.WriteLine($"вывод: {result}");
+
---
+++ Task12/Program.cs
@@ -0,0 +1,42 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1%num2;
+if (result>0)
+{
+    Console.WriteLine($"Не кратное. Остаток от деления: {result}");
+} 
+else
+{
+    Console.WriteLine($"Числа кратны.");
+} 
+
+// int DivNum(int number1, int number2)
+// {
+//     int result = number1%number2;
+//     return result;
+// }
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = DivNum(num1, num2);
+// if (result>0)
+// {
+//     Console.WriteLine($"Не кратное. Остаток от деления: {result}");
+// } 
+// else
+// {
+//     Console.WriteLine($"Числа кратны.");
+// } 

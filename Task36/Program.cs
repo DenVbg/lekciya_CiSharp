8d87b4b1203d2ca77a6794def9a7bf65eb7cbfb9
denis <den_vbg@mail.ru>
2022-11-13 20:21:42 +0300

    add Task 34,36,38

---
+++ Task36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int sizeDigit = 8; // количество элементов массива
+
+int[] SizeArray(int size)
+{
+    int[] array = new int[size]; //
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{   
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i<array.Length-1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int[] sizeArray = SizeArray(sizeDigit);
+Console.WriteLine("Вывод массива из 8 элементов заполненный псевдослучайными числами : ");
+PrintArray(sizeArray);
